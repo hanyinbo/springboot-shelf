7701520cab45bd1cb1e66e27a758c510b74c2698
hyb <13407937436@163.com>
2021-09-13 13:16:01 +0800

    提取MInfoItem公共字段

--- aison-service/src/main/java/com/aison/entity/MInfoItem.java
+++ aison-service/src/main/java/com/aison/entity/MInfoItem.java
@@ -1,18 +1,14 @@
 package com.aison.entity;
 
-import com.alibaba.fastjson.annotation.JSONField;
+import com.aison.bean.BaseEntity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.util.Date;
 
 @Data
 @TableName(value = "m_info_item")//指定表名
-public class MInfoItem {
+public class MInfoItem extends BaseEntity {
     @TableId
     @TableField(value = "oid")
     private Long oid;
@@ -22,15 +18,4 @@ public class MInfoItem {
     private Integer level;
     @TableField(value = "r_oid")
     private Long roid;
-    @TableField(value = "creatime")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JSONField(format = "yyyy-MM-dd HH:mm:ss")
-    private Date creatime;
-    @TableField(value = "creator")
-    private String creator;
-    @TableField(value = "updatime")
-    private Date updatime;
-    @TableField(value = "updator")
-    private String updator;
 }
